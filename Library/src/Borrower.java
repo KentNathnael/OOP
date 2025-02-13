463d88e305f9eb254bea5f140649cca1f9d29295
Kent <kentnathanael06@gmail.com>
2025-02-13 09:30:21 +0700

    Updated Code

--- Library/src/Borrower.java
+++ Library/src/Borrower.java
@@ -1,24 +1,39 @@
-import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Borrower{
     String nama;
+    private List<Book>  borrowedBook;
     public Borrower(String nama){
         this.nama = nama;
+        this.borrowedBook = new ArrayList<>();
     }
     public String getNama(){
         return nama;
     }
 
     public void displayBorrowedBooks(){
-        System.out.println(getNama() + "'s Borrowed Books: ");
-        Book displaybook = new Book();
-        displaybook.
+        if(borrowedBook.isEmpty()){
+            System.out.println("No book is borrowed");
+        }
+        else{
+            System.out.println(getNama() + "'s Borrowed Books: ");
+            for(Book book : borrowedBook){
+                System.out.println("Title: " + book.getJudul());
+                System.out.println("ISBN: " + book.getISBN());
+                System.out.println("Author: " + book.getAuthor().getNama());
+                System.out.println("Biography: " + book.getAuthor().getDescription());
+                System.out.println("----------------------------");
+            }
+        }
     }
 
-    public void printBook(){
-        System.out.println("Title: " + getJudul());
-        System.out.println("ISBN: " + getISBN());
-        System.out.println("Author: " + author.getNama());
-        System.out.println("Biography: " + author.getDescription());
+    public void borrowBook(Book book){
+        borrowedBook.add(book);
     }
+
+    public void returnBook(Book book){
+        borrowedBook.remove(book);
+    }
+
 }
--- Library/src/Library.java
+++ Library/src/Library.java
@@ -2,32 +2,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    List<Book> books;
-    List<Book> borrowed;
+    private List<Book> books;
     public Library(){
         books = new ArrayList<>();
-        borrowed = new ArrayList<>();
     }
     public void addBook(Book book){
         books.add(book);
         System.out.println("Book added to library : " + book.getJudul());
     }
     public void borrowBook(Borrower borrower, Book book){
-        borrowed.add(book);
-        books.remove(book);
-        System.out.println(borrower.getNama() + "borrowed: " + book.getJudul());
+        if(books.isEmpty()){
+            System.out.println("There is no book to be borrowed");
+        }
+        else{
+            borrower.borrowBook(book);
+            books.remove(book);
+            System.out.println(borrower.getNama() + " borrowed: " + book.getJudul());
+        }
     }
 
     public void returnBook(Borrower borrower, Book book){
         books.add(book);
-        borrowed.remove(book);
-        System.out.println(borrower.getNama() + "returned: " + book.getJudul());
+        borrower.returnBook(book);
+        System.out.println(borrower.getNama() + " returned: " + book.getJudul());
     }
 
     public void displayLibraryBooks(){
-        System.out.println("Title: " + books.);
-        System.out.println("ISBN: " + getISBN());
-        System.out.println("Author: " + author.getNama());
-        System.out.println("Biography: " + author.getDescription());
+        if(books.isEmpty()){
+            System.out.println("There is no book to be borrowed");
+        }
+        else{
+            System.out.println("Book in Library: ");
+            for(Book book : books){
+                System.out.println("Title: " + book.getJudul());
+                System.out.println("ISBN: " + book.getISBN());
+                System.out.println("Author: " + book.getAuthor().getNama());
+                System.out.println("Biography: " + book.getAuthor().getDescription());
+                System.out.println("----------------------------");
+            }
+        }
     }
 }
